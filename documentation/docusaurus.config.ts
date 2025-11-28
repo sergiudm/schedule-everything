41dbca6abd6da7559227a30164eb3c75351ac5c3
Sergiu <sergiudm@outlook.com>
2025-11-29 04:46:01 +0800

    fix: update base URL and project name in Docusaurus config for consistency

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -19,12 +19,12 @@ const config: Config = {
   url: 'https://sergiudm.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/schedule_management/',
+  baseUrl: '/schedule_everything/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sergiudm', // Usually your GitHub org/user name.
-  projectName: 'schedule_management', // Usually your repo name.
+  projectName: 'schedule_everything', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -55,7 +55,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/sergiudm/schedule_management/tree/main/documentation/',
+            'https://github.com/sergiudm/schedule_everything/tree/main/documentation/',
         },
         blog: {
           showReadingTime: true,
@@ -66,7 +66,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/sergiudm/schedule_management/tree/main/documentation/',
+            'https://github.com/sergiudm/schedule_everything/tree/main/documentation/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -103,7 +103,7 @@ const config: Config = {
           position: 'right',
         },
         {
-          href: 'https://github.com/sergiudm/schedule_management',
+          href: 'https://github.com/sergiudm/schedule_everything',
           label: 'GitHub',
           position: 'right',
         },
@@ -134,11 +134,11 @@ const config: Config = {
           items: [
             {
               label: 'GitHub Issues',
-              href: 'https://github.com/sergiudm/schedule_management/issues',
+              href: 'https://github.com/sergiudm/schedule_everything/issues',
             },
             {
               label: 'Discussions',
-              href: 'https://github.com/sergiudm/schedule_management/discussions',
+              href: 'https://github.com/sergiudm/schedule_everything/discussions',
             },
           ],
         },
@@ -147,11 +147,11 @@ const config: Config = {
           items: [
             {
               label: 'GitHub Repository',
-              href: 'https://github.com/sergiudm/schedule_management',
+              href: 'https://github.com/sergiudm/schedule_everything',
             },
             {
               label: 'PyPI Package',
-              href: 'https://pypi.org/project/schedule-management/',
+              href: 'https://pypi.org/project/schedule-everything/',
             },
           ],
         },
